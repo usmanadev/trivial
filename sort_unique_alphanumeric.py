6a54e86859a854f251065c827af9dc6e3050153f
usmanadev <usmana.dev@gmail.com>
2021-03-26 20:09:24 +0500

    Updated alphanumeric set sort

---
+++ sort_unique_alphanumeric.py
@@ -0,0 +1,8 @@
+def sort_unique_alphanumeric(str):
+    """Removes all duplicate whitespace separated words from the string and sorts it lexicographically.
+    """
+    split_str = str.split()
+    split_str.sort()
+    unique_split_str = set(split_str)
+    ' '.join(unique_split_str)
+    return unique_split_str
